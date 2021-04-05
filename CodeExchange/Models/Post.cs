405ee2c52915bfc053ed75e689d5fe9925e0c0ea
tien96ng <tien96ng@gmail.com>
2021-04-05 14:13:41 -0700

    Create Post.cs Model
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>
    Co-authored-by: Connor Burgess <connorburgesscodes@gmail.com>
    Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

---
+++ CodeExchange/Models/Post.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace CodeExchange.Models
+{
+  public class Post
+  {
+    public Post()
+    {
+      this.JoinEntities = new HashSet<AppUserForumPost>();
+    }
+
+    public int PostId { get; set; }
+    public int Likes { get; set; }
+    public int Dislikes { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    // public string IpAddress { get; set; }
+    // public bool isComment { get; set; }
+
+    public List<Post> Comments { get; set; }
+    public virtual ICollection<AppUserForumPost> JoinEntities { get; set; }
+  }
+}
