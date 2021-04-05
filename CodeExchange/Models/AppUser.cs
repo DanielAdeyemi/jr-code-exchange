267259adf9e8e36838fc96fc8dc2e45d81f5db45
tien96ng <tien96ng@gmail.com>
2021-04-05 14:02:13 -0700

    Create AppUser.cs Model
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>
    Co-authored-by: Connor Burgess <connorburgesscodes@gmail.com>
    Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

---
+++ CodeExchange/Models/AppUser.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace CodeExchange.Models
+{
+  public class AppUser
+  {
+    public AppUser()
+    {
+      this.JoinEntities = new HashSet<AppUserForumPost>();
+    }
+
+    public int AppUserId { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public int Rep { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime LastActive { get; set; }
+    public HashSet<int> Followers { get; set; } // Each user's Id
+    public HashSet<int> Following { get; get; }
+
+    public string IpAddress { get; set; }
+
+    public virtual ICollection<AppUserForumPost> JoinEntities { get; set; }
+  }
+}
