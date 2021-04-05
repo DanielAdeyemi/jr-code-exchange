08ac8f73c48df4d2dc0ca9261946637eb7508c89
tien96ng <tien96ng@gmail.com>
2021-04-05 10:49:30 -0700

    Create Startup.cs and Edit Program.cs

---
+++ CodeExchange/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CodeExchange.Helpers;
+using CodeExchange.Services;
+using CodeExchange.Models;
+using CodeExchange.Interfaces;
+
+namespace CodeExchange
+{
+	public class Startup
+  {
+    public Startup(IConfiguration configuration)
+  	{
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // Section for JWT
+      // services.AddCors();
+      // services.AddControllers();
+
+      // // configure strongly typed settings object
+      // services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+      // // configure DI for application services
+      // services.AddScoped<IUserService, UserService>();
+
+      services.AddDbContext<LocalBusinessContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      // app.UseHttpsRedirection();
+
+      // global cors policy
+      // app.UseCors(x => x
+      //   .AllowAnyOrigin()
+      //   .AllowAnyMethod()
+      //   .AllowAnyHeader());
+
+      // custom jwt auth middleware
+      // app.UseMiddleware<JwtMiddleware>();
+
+      app.UseRouting();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
