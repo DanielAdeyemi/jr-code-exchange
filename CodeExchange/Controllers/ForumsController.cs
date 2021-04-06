8e233e61a875fe161a934c1df3ef14a503d68963
Connor Burgess <connorburgesscodes@gmail.com>
2021-04-06 14:29:40 -0700

    start to implement controllers

---
+++ CodeExchange/Controllers/ForumsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using CodeExchange.Models;
+
+namespace CodeExchange.Controllers
+{
+  [Authorize]
+  public class ForumsController : Controller
+  {
+    private readonly CodeExchangeContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public AppUsersController(UserManager<ApplicationUser> userManager, CodeExchangeContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return RedirectToAction("Index");
+    }
+  }
+}
--- CodeExchange/Controllers/HomeController.cs
+++ CodeExchange/Controllers/HomeController.cs
@@ -1,13 +1,38 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using CodeExchange.Models;
 
 namespace CodeExchange.Controllers
 {
+  [Authorize]
   public class HomeController : Controller
   {
-    [HttpGet("/")]
+    private readonly CodeExchangeContext _db;
+    public HomeController(UserManager<ApplicationUser> userManager, CodeExchangeContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    [AllowAnonymous]
     public ActionResult Index()
     {
-      return View();
+      // Grab _Db of forums and Include Posts sorted by DateTime
+      // Pass both into view. Forums will be integrated into sidebar 
+      ICollection<Forum> model = _db.Forums.ToList();
+      ViewBag.postByDate = _db.Posts.ToList().OrderByDescending(e => e.CreationDate);
+      ViewBag.postByPopularity = _db.
+
+      // (Implement on CSHTML) =
+      // Scrolling list of most popular posts (stretch)
+      // OR button at button to load next 10 most popular posts
+      return View(model);
     }
   }
 }
